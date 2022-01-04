23f9b2bfd588cdf8fbba44c33db73f2c4645280c
nadeemsoft <nadeemashrafi38@gmail.com>
2022-01-04 09:16:28 +0530

    data binding

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  productList:string[] = ['Apple','Mango','Orange'];
+  updatedProductList:string[] = [];
+  isActive:boolean = true;
+  message:string = 'Hi I am angular developer'
+  imageURL:string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    setTimeout(() => {
+      this.isActive = false
+    }, 3000);
   }
 
+  ngOnInit(): void {}
+
+  showImage(){
+    alert('clicked')
+    this.imageURL = 'https://static.digit.in/product/284c81498a93a82db7b5db6600494c20bee4f18e.jpeg';
+    this.productList.push('Nazeer');
+    this.productList.push('Javascript');
+    console.log(this.productList)
+    this.updatedProductList = this.productList;
+  }
+
+  showMSG(){
+    alert('HI am alert')
+  }
 }
