1517ff50d12e21def2e94370edbc018ece5bcea4
nadeemsoft <nadeemashrafi38@gmail.com>
2022-01-19 09:22:13 +0530

    child to parent

--- src/app/components/Component-Interaction/child/child.component.ts
+++ src/app/components/Component-Interaction/child/child.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit ,OnChanges{
   @Input() myMSG!:string;
+  @Output() messageFromChild:any = new EventEmitter<any>();
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+      console.log(changes.myMSG.currentValue)
+      console.log(changes.myMSG)
   }
+
+  ngOnInit(): void {
+    setTimeout(()=>{
+      console.log("setTimeout is executed")
+    },3000)
+  }
+  sendData(inputValue:any){
+    console.log('method called'+inputValue);
+    this.messageFromChild.emit(inputValue);
+    }
 
 }
--- src/app/components/Component-Interaction/parent/parent.component.ts
+++ src/app/components/Component-Interaction/parent/parent.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements OnInit ,AfterViewInit,AfterViewChecked,OnDestroy{
   message:string = "Hi i am learing angular."
-  constructor() { }
-
-  ngOnInit(): void {
+  dataFromChild:any;
+  constructor() { 
+    console.log("constructor is called");
   }
 
+  ngOnInit(): void {
+    console.log("ngOnInit is called");
+    localStorage.setItem('token','hjgasdkjhgaKJGadgAGKJGah');
+  }
+  receiveData(event:any){
+    console.log(event);
+    this.dataFromChild = event;
+  }
+  ngAfterViewInit(): void {
+      console.log("ngAfterViewInit is called");
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked is called");
+  }
+  ngOnDestroy(): void {
+      console.log("ngOnDestroy is called");
+      localStorage.clear();
+  }
 }
